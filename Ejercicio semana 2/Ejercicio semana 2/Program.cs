720d9d7e228e22c71f76c2b0826db14df6e00ec7
George Jimy <knivespeans@gmail.com>
2023-09-05 20:10:26 -0500

    Add Shape, Circle, Rectangle

---
+++ Ejercicio semana 2/Ejercicio semana 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_semana_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape s = new Circle(10);
+
+            Console.WriteLine($"El área del {s.GetName()} es : {s.CalculateArea()}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ejercicio semana 2/Ejercicio semana 2/Rectangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_semana_2
+{
+    class Rectangle : Shape
+    {
+        private float b;
+        private float h;
+
+        public Rectangle(float b, float h)
+        {
+            this.name = "Rectangle";
+            this.b = b;
+            this.h = h;
+        }
+
+        public override float CalculateArea()
+        {
+            return b * h;
+        }
+    }
+}
